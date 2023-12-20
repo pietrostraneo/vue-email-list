75e257195a69721b7daba298c0a76be0b100e703
pietrostraneo <straneo_p@yahoo.it>
2023-12-20 14:20:53 +0100

    ESERCIZIO CONSEGNATO

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        
+        mail: []
       }
     },
     created(){
-        
+        for(let i = 0; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+                this.mail.push(response.data.response)
+            })
+        }
     },
     methods: {
 
